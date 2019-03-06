ca0b675cdd49bf399f07ea997fb5be78ec390dc7
zkr12123 <48183099+zkr12123@users.noreply.github.com>
2019-03-06 20:52:18 +0000

    test

--- test-loop.cpp
+++ test-loop.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+bool check_contain(int n, const std::vector<int>& v);
+
 int main(){
   std::vector<int> attempt, sequence, black, white;
 
@@ -28,11 +30,14 @@ int main(){
 
   for(int i = 0; i < attempt.size(); i++){
     for(int j = 0; j < sequence.size(); j++){
-      for(int k = 0; k < black.size(); k++){
-        if((attempt[i] == sequence[j]) && (j != black[k])){ //probelm
-          white.push_back(j); //contains repeated white_hits
-        }
+      if((attempt[i] == sequence[j]) && (!check_contain(j, black))){
+        white.push_back(j);
       }
+      //for(int k = 0; k < black.size(); k++){
+        //if((attempt[i] == sequence[j]) && (j != black[k])){ //probelm
+          //white.push_back(j); //contains repeated white_hits
+        //}
+      //}
     }
   }
 
@@ -47,4 +52,14 @@ int main(){
   return 0;
 
 
+}
+
+bool check_contain(int n, const std::vector<int>& v){
+  bool contain = false;
+  for(int i = 0; i < v.size() && !contain; i++){
+    if(n == v[i]){
+      contain = true;
+    }
+  }
+  return contain;
 }
