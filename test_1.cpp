81dcd71d41ce538a8fd396e6af79d9d70bcc3c62
zkr12123 <48183099+zkr12123@users.noreply.github.com>
2019-03-09 18:41:01 +0000

    update

--- test_1.cpp
+++ test_1.cpp
@@ -1,47 +1,107 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
+#include <ctime>
 
-void give_feedback(const std::vector<int>& attempt, int& black_hits, int& white_hits){
-    std::vector<int> black;
+//this tests random pumping
 
-    for(int i = 0; i < attempt.size(); i++){
-      if(attmpt[i] == sequence[i]){
-        black_hits++;
-        black.push_back(i);
-      }
-    }
+void set_random_seed();
+int randn(int n);
+int pow(int a, int b);
 
+struct mm_solver{
 
-    for(int i = 0; i < attempt.size(); i++){
-      for(int j = 0; j < sequence.size(); j++){
-        for(int k = 0; k < black.size(); k++){
-          if((attempt[i] == sequence[j]) && (j != black[k])){
-            white.push_back(j); //contains repeated white_hits
+    /// this member function sets the values for the member data
+    /// representing the lenght of the code
+    /// and the number of symbols (the symbols will be 0 to i_num - 1)
+    /// (this should be a constructor in proper OOP but ok)
+    /// do not alter the function interface (name, parameter list, void return)
+    void init(int i_length, int i_num){
+        length = i_length;
+        num = i_num;
+        std::vector<int> tmp;
+        for(int i = 0; i < length; i++){
+          tmp.push_back(randn(num));
+        }
+        set.push_back(tmp);
+
+        while(set.size() < pow(num, length)){
+
+          std::vector<int> tmp;
+          bool repeated = false;
+          for(int i = 0; i < length; i++){
+            tmp.push_back(randn(num));
+          }
+
+          for(int i = 0; i < set.size() && !repeated; i++){
+            if(tmp == set[i]){
+              repeated = true;
+            }
+          }
+
+          if(!repeated){
+            set.push_back(tmp);
           }
         }
-      }
+
+
+
+
+        /// you can include additional implementation lines here if needed
+
     }
 
-    if(white.size() > 0){
-      //std::vector<int> truwhite;
-      //truwhite.push_back(white[0]);
-      white_hits++;
-      for(int i = 1; i < white.size(); i++){
+    /// this member function creates an attempt to find the right code
+    /// (see the other examples provided for clarifications)
+    /// do not alter the function interface (name, parameter list, void return)
+    void create_attempt(std::vector<int>& attempt){
+        /// write your implementation here
+    }
 
-        bool repeated = false;
+    /// this member function acquires the feedback about a certain attempt
+    /// (see the other examples provided for clarifications)
+    /// do not alter the function interface (name, parameter list, void return)
+    void learn(std::vector<int>& attempt, int black_hits, int white_hits){
+        /// write your implementation here
+    }
 
-        for(int j = 0; j < white.size() && !repeated; j++){
-          if(white[i] == white[j]){
-            repeated = true;
-          }
-        }
+    int length;
+    int num;
+    std::vector<std::vector<int>> set;
 
-        if(!repeated){
-          //truwhite.push_back(white[i]);
-          white_hits++;
-        }
+    /// you may add other member functions and member data as needed
+    /// (keep in mind the distinction between member function variables
+    /// and member data of the struct)
 
-      }
+};
+
+int main(){
+  std::cout << "initializing" << std::endl;
+  mm_solver test;
+  test.init(6, 6);
+  std::cout << "size: " << test.set.size() << std::endl << std::endl;
+  for(int i = 0; i < test.set.size(); i++){
+    for(int j = 0; j < test.set[i].size(); j++){
+          std::cout << test.set[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  return 0;
 }
 
+void set_random_seed(){
+    std::srand(std::time(0));
+}
+
+int randn(int n){
+    return std::rand() % n;
+}
+
+int pow(int a, int b){
+  int n = a;
+  for(int i = 1; i < b; i++){
+    n = n * a;
+  }
+  return n;
 }
---
+++ test_2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+//this tests pool_generator
+
+std::vector<int> pool_generator(int length, int num, int count);
+int pow(int a, int b);
+
+int main(){
+  std::cout << "pumping vectors..." << std::endl;
+  int num = 9;
+  int length = 9;
+  std::vector<std::vector<int>> pool;
+
+  for(int i = 0; i < pow(num, length); i++){
+    pool.push_back(pool_generator(length, num, i));
+  }
+
+  std::cout << "pool size: " << pool.size() << std::endl;
+
+  for(int i = 0; i < pool.size(); i++){
+    for(int j = 0; j < pool[i].size(); j++){
+      std::cout << pool[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+
+
+}
+
+/*int main(){
+  int n;
+  std::cout << "input n" << std::endl;
+  std::cin >> n;
+  std::vector<int> v = pool_generator(15, 15, n);
+  for(int i = 0; i < v.size(); i++){
+    std::cout << v[i] << " ";
+  }
+  std::cout << std::endl;
+}*/
+
+
+
+std::vector<int> pool_generator(int length, int num, int count){
+  //int set_length = pow(num, length);
+  std::vector<int> v, tmp;
+  //int tmp = count;
+  for(int i = 0; i < length; i++){
+    v.push_back(0);
+  }
+
+  int quotient = count;
+  int remainder;
+  while(quotient != 0){
+    remainder = quotient % num;
+    quotient = (int) quotient / num;
+    tmp.push_back(remainder);
+  }
+
+  for(int i = v.size() - 1, j = 0; j < tmp.size(); i--, j++){
+    v[i] = tmp[j];
+  }
+
+  return v;
+}
+
+int pow(int a, int b){
+  int n = a;
+  for(int i = 1; i < b; i++){
+    n = n * a;
+  }
+  return n;
+}
